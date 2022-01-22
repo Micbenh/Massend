56c45ab20da7b1453f4b1a7ea3722c233003a1bf
Michael <t-mibenh@microsoft.com>
2022-01-22 10:40:26 +0200

    added label headers

--- Massend/app.py
+++ Massend/app.py
@@ -3,11 +3,21 @@ import tkinter as tk
 
 class MainWindow:
     def __init__(self, master):
+        # Window design
         self.master = master
         self.master.title("Hello World")
         self.top_frame = tk.Frame(self.master)
-        self.top_frame.grid(row = 0, column = 0)
+        self.top_frame.grid(row = 0, column = 0, padx=10)
 
+        self.group_name = tk.Label(self.top_frame, text="Group Name ")
+        self.group_name.grid(row = 0, column = 0)
+
+        self.distribution_group = tk.Label(self.top_frame, text="Distribution Group")
+        self.distribution_group.grid(row = 0, column = 1)
+
+
+
+        
 
 
 
